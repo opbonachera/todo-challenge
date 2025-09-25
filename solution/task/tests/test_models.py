c9a12fb04ff3bbe262115f18c82797260839e8e1
opbonachera <obonachera@gmail.com>
2025-09-25 09:13:41 -0300

    feat: added task model tests

---
+++ solution/task/tests/test_models.py
@@ -0,0 +1,110 @@
+# your_app/tests/test_models.py
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError, transaction
+
+from ..models import Task
+
+
+User = get_user_model()
+
+
+class TaskModelTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username="testuser", email="t@example.com", password="password")
+
+    def test_create_valid_task_and_defaults(self):
+        """ default values """
+        t = Task.objects.create(title="My task", created_by=self.user)
+        self.assertEqual(t.title, "My task")
+        self.assertFalse(t.completed, "completed should default to False")
+        self.assertFalse(t.deleted, "deleted should default to False")
+        self.assertEqual(t.priority, 1, "priority default should be 1")
+        self.assertEqual(t.tags, [], "tags default should be an empty list")
+        self.assertIsNone(t.updated_at, "updated_at default is None")
+
+        now = timezone.now()
+        delta_secs = abs((t.created_at - now).total_seconds())
+        self.assertLess(delta_secs, 5, "created_at should be set to current time on save")
+
+    def test___str__returns_title(self):
+        t = Task.objects.create(title="Hello title", created_by=self.user)
+        self.assertEqual(str(t), "Hello title")
+
+    def test_title_required_and_max_length(self):
+
+        t_empty = Task(title="", created_by=self.user)
+        with self.assertRaises(ValidationError):
+            t_empty.full_clean() # title must be non-empty
+
+        t_long = Task(title="a" * 256, created_by=self.user)
+        with self.assertRaises(ValidationError): 
+            t_long.full_clean()
+
+    def test_description_max_length(self):
+        t_long = Task(title="long description", description="a" * 261, created_by=self.user)
+        with self.assertRaises(ValidationError): 
+            t_long.full_clean()
+
+
+    def test_priority_validators(self):
+        # priority must be between 1 and 5
+        t_low = Task(title="low", priority=0, created_by=self.user)
+        with self.assertRaises(ValidationError):
+            t_low.full_clean()
+
+        t_high = Task(title="high", priority=6, created_by=self.user)
+        with self.assertRaises(ValidationError): # priority over 5
+            t_high.full_clean()
+
+        t_min = Task.objects.create(title="min", priority=1, created_by=self.user)
+        self.assertEqual(t_min.priority, 1)
+        t_max = Task.objects.create(title="max", priority=5, created_by=self.user)
+        self.assertEqual(t_max.priority, 5)
+
+    def test_tags_accepts_dict_and_lists(self):
+        # tags can be any JSON-serializable value
+        d = {"k": "v"}
+        arr = ["a", 1, {"b": 2}]
+        t1 = Task.objects.create(title="dict-tags", created_by=self.user, tags=d)
+        t2 = Task.objects.create(title="list-tags", created_by=self.user, tags=arr)
+        self.assertEqual(t1.tags, d)
+        self.assertEqual(t2.tags, arr)
+
+        # non json serializable values
+        t = Task(title="bad-tags", created_by=self.user)
+        t.tags = set([1, 2]) 
+
+        with self.assertRaises(TypeError):
+            t.save()
+
+    def test_created_by_fk_constraint_and_default_0_behavior(self):
+        # creating a Task without a user
+        t = Task(title="no-user-provided")
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                t.save()
+
+        # # non-existent user id
+        # maybe creating an user and then deleting it to get a non-existent id would work
+        # but it throws value error not integrity error
+        # self.user.delete()
+        # t2 = Task(title="bad-user-id", created_by=self.user)
+        # with self.assertRaises(IntegrityError):
+        #     with transaction.atomic():
+        #         t2.save()
+
+    def test_cascade_delete_user_deletes_tasks(self):
+        """on_delete=models.CASCADE -> deleting the user should delete their tasks."""
+        t = Task.objects.create(title="to-be-deleted", created_by=self.user)
+        self.user.delete()
+        self.assertFalse(Task.objects.filter(pk=t.pk).exists())
+
+
+    def test_set_and_persist_updated_at(self):
+        now = timezone.now()
+        t = Task.objects.create(title="with-updated-at", created_by=self.user, updated_at=now)
+        self.assertEqual(t.updated_at.replace(microsecond=0), now.replace(microsecond=0))
