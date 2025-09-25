93352222baee5e98c399ea73bd0576d0f3e809b3
opbonachera <obonachera@gmail.com>
2025-09-24 22:53:00 -0300

    fix: incorrect urls

--- solution/core/settings.py
+++ solution/core/settings.py
@@ -2,26 +2,28 @@ from pathlib import Path
 from datetime import timedelta
 import environ
 
-
 env = environ.Env(
     DEBUG=(bool, False)  
 )
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 environ.Env.read_env(BASE_DIR / ".env")
-
+API_VERSION = 'v1'
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 AUTH_USER_MODEL = "authentication.User"
-
+ROOT_URLCONF = "core.urls"
+STATIC_URL = "/static/"
+APPEND_SLASH = False
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.contenttypes',
+    'drf_standardized_errors',
     'rest_framework.authtoken',
     'django.contrib.auth',
     'core',
@@ -36,6 +38,7 @@ MIDDLEWARE = MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "drf_standardized_errors.handler.exception_handler",
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
@@ -43,18 +46,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
-
 SIMPLE_JWT = {
         'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20), 
         'REFRESH_TOKEN_LIFETIME': timedelta(days=1),   
         'ROTATE_REFRESH_TOKENS': True,                  
         'BLACKLIST_AFTER_ROTATION': True,               
     }
-
-ROOT_URLCONF = "core.urls"
-STATIC_URL = "/static/"
-APPEND_SLASH = False
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DATABASES = {
     'default': {
         'ENGINE': env("DB_ENGINE"),
--- solution/core/urls.py
+++ solution/core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-
-API_VERSION = 'v1'
+from core.settings import API_VERSION
 
 urlpatterns = [
-    path(f'api/{API_VERSION}/', include('task.urls')),
+    path(f'api/{API_VERSION}/task/', include('task.urls')),
     path(f'api/{API_VERSION}/auth/', include('authentication.urls')),
 ]
